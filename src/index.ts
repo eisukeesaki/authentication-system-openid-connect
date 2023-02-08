641b25cf0c89ecbb59c97d6a2462fc06239c3c64
Eisuke Esaki <eisukeesaki@gmail.com>
2023-02-07 22:54:36 -0800

    log environment

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+console.log(`
+ENVIRONMENT:
+    HOST: ${process.env.HOST}
+    PORT: ${process.env.PORT}
+    OAUTH_CLIENT_ID: ${process.env.OAUTH_CLIENT_ID}
+    OAUTH_CLIENT_SECRET: ${process.env.OAUTH_CLIENT_SECRET}
+`);
+
 app.listen(process.env.PORT, () => {
   console.log(`Express started on port ${process.env.PORT}`);
 });
