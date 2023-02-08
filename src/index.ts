bca77cc3c60c5758de3d4b00bf12cd94bdfbf12e
Eisuke Esaki <eisukeesaki@gmail.com>
2023-02-08 15:09:52 -0800

    implemented route /

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import mustacheExpress from 'mustache-express';
 
-const app = express();
-
+/**
+ * log server environment info
+ */
 console.log(`
 ENVIRONMENT:
     HOST: ${process.env.HOST}
@@ -10,6 +12,26 @@ ENVIRONMENT:
     OAUTH_CLIENT_SECRET: ${process.env.OAUTH_CLIENT_SECRET ? true : false}
 `);
 
+const app = express();
+
+/**
+ * set up mustache-express as templating engine
+ */
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+/**
+ * route /
+ * render home page containing link to initiate OIDC flow
+ */
+app.get('/', (req: Request, res: Response) => {
+  res.render('index');
+});
+
+/**
+ * Start a TCP server listening for connections on the given port and host
+ */
 app.listen(process.env.PORT, () => {
   console.log(`Express started on port ${process.env.PORT}`);
 });
